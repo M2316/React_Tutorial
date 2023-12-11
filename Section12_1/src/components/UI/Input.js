7f814ee29dd0649fbcc23bcfb39c6fdb801f6038
M2316 <031pjs@gmail.com>
2023-12-11 10:14:54 +0900

    Section12-1 강의 따라하기

---
+++ Section12_1/src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
