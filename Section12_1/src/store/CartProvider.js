24a4073820dc68c5b40f3087ed1dbe985a4855e5
M2316 <031pjs@gmail.com>
2023-12-15 00:10:33 +0900

    Section12-1 강사님과 같이진행하는 프로젝트.. 진행중...

---
+++ Section12_1/src/store/CartProvider.js
@@ -0,0 +1,18 @@
+import CartContext from "./cart-context";
+const CartProvider = props =>{
+
+    const addItemToCartHandler = item =>{};
+    const removeFromCartHandler = item =>{};
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem : addItemToCartHandler,
+        removeItem: removeFromCartHandler
+    }
+    return <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+}
+
+export default CartProvider;
